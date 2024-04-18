431441bfca03d7fc222953316d75f8b5dee8b7ff
jinjernot <norindes@gmail.com>
2024-04-17 20:05:59 -0600

    ignore

---
+++ config.py
@@ -0,0 +1,8 @@
+user_hp = "gilberto.garcia.villanueva@hp.com"
+pass_hp = "xxx"
+
+user_etma = "hp_recovery"
+pass_etma =  "xxx"
+
+etma_url = "https://hp-tmsweb-itg.inc.hp.com/"
+etma_inbox_url = "https://hp-tmsweb-itg.inc.hp.com/interface/inbox/inboxbase.asp?ownerid=2827"
